b44d168ded7746fdea0a942c316ac0152cf58ab9
Aiden Mathews <aiden.mathews2002@gmail.com>
2023-07-11 17:53:17 -0500

    Floating cube

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -1,7 +1,10 @@
 package engineTester;
 
 import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
 
+import entities.Camera;
+import entities.Entity;
 import models.RawModel;
 import models.TexturedModel;
 import renderEngine.DisplayManager;
@@ -16,36 +19,102 @@ public class MainGameLoop {
 		
 		DisplayManager.createDisplay();
 		Loader loader = new Loader();
-		Renderer renderer = new Renderer();
 		StaticShader shader = new StaticShader();
+		Renderer renderer = new Renderer(shader);
 		
-		float[] vertices = {
-				-0.5f, 0.5f, 0f,	//V0
-				-0.5f, -0.5f, 0f,	//V1
-				0.5f, -0.5f, 0f,	//V2
-				0.5f, 0.5f, 0f,		//V3
-		};
-		
-		int[] indices = {
-				0,1,3,	//Top left triangle (V0,V1,V3)
-				3,1,2	//Bottom right triangle (V3,V1,V2)
+		float[] vertices = {			
+				-0.5f,0.5f,-0.5f,	
+				-0.5f,-0.5f,-0.5f,	
+				0.5f,-0.5f,-0.5f,	
+				0.5f,0.5f,-0.5f,		
+				
+				-0.5f,0.5f,0.5f,	
+				-0.5f,-0.5f,0.5f,	
+				0.5f,-0.5f,0.5f,	
+				0.5f,0.5f,0.5f,
+				
+				0.5f,0.5f,-0.5f,	
+				0.5f,-0.5f,-0.5f,	
+				0.5f,-0.5f,0.5f,	
+				0.5f,0.5f,0.5f,
+				
+				-0.5f,0.5f,-0.5f,	
+				-0.5f,-0.5f,-0.5f,	
+				-0.5f,-0.5f,0.5f,	
+				-0.5f,0.5f,0.5f,
+				
+				-0.5f,0.5f,0.5f,
+				-0.5f,0.5f,-0.5f,
+				0.5f,0.5f,-0.5f,
+				0.5f,0.5f,0.5f,
+				
+				-0.5f,-0.5f,0.5f,
+				-0.5f,-0.5f,-0.5f,
+				0.5f,-0.5f,-0.5f,
+				0.5f,-0.5f,0.5f
+				
 		};
 		
 		float[] textureCoords = {
-				0,0,	//V0
-				0,1,	//V1
-				1,1,	//V2
-				1,0,	//V3
+				
+				0,0,
+				0,1,
+				1,1,
+				1,0,			
+				0,0,
+				0,1,
+				1,1,
+				1,0,			
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0
+
+				
+		};
+		
+		int[] indices = {
+				0,1,3,	
+				3,1,2,	
+				4,5,7,
+				7,5,6,
+				8,9,11,
+				11,9,10,
+				12,13,15,
+				15,13,14,	
+				16,17,19,
+				19,17,18,
+				20,21,23,
+				23,21,22
+
 		};
 		
 		RawModel model = loader.loadToVAO(vertices,textureCoords,indices);
-		ModelTexture texture = new ModelTexture(loader.loadTexture("Amongus_Seal"));
-		TexturedModel texturedModel = new TexturedModel(model, texture);
+		TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("Amongus_Seal")));
+		
+		Entity entity = new Entity(staticModel, new Vector3f(0,0,-5),0,0,0,1);
+		
+		Camera camera = new Camera();
 		
 		while(!Display.isCloseRequested()) {
+			entity.increaseRotation(1, 1, 0);
+			camera.move();
 			renderer.prepare();
 			shader.start();
-			renderer.render(texturedModel);
+			shader.loadViewMatrix(camera);
+			renderer.render(entity,shader);
 			shader.stop();
 			DisplayManager.updateDisplay();
 		}
